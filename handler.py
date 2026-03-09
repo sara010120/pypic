1ce1fef6d5616c663e0e3077eb6356943e871188
sara010120 <sara01012026@outlook.com>
2026-03-09 00:06:07 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -550,7 +550,7 @@ def handle_rerun_ut(gh_repo, pr, comment, user_perms, test_spec, token):
         return False
 
     # Detect suite and runner
-    # suite, runner_label, use_deepep, err = detect_cuda_suite(resolved_path)
+    suite, runner_label, use_deepep, err = detect_cuda_suite(resolved_path)
     if err:
         comment.create_reaction("confused")
         pr.create_issue_comment(f"❌ {err}")
@@ -563,7 +563,7 @@ def handle_rerun_ut(gh_repo, pr, comment, user_perms, test_spec, token):
 
     print(
         f"Resolved: file={resolved_path}, selector={test_selector}, "
-        f"suite={}, runner={}, deepep={}, command='{test_command}'"
+        f"suite={suite}, runner={runner_label}, deepep={use_deepep}, command='{test_command}'"
     )
 
     try:
