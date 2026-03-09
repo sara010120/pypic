423a60e44d6174fec6915a165c0ce8bd5ca2d870
sara010120 <sara01012026@outlook.com>
2026-03-08 20:28:57 -0700

    Update pytest.py

--- test/registered/pytest.py
+++ test/registered/pytest.py
@@ -1,4 +1,7 @@
 import subprocess
 
-subprocess.run("ls -l", shell=True)
-subprocess.run("echo 1 >/tmp/1", shell=True)
+class CommandRunner:
+
+    def run_cmd(self):
+        result = subprocess.run("echo 1>/tmp/1", shell=True, capture_output=True, text=True)
+        return result.stdout
